0b0f63c4c2014927ee7f0ac6f624a15fd849b210
MainKernel <86931698+MainKernel@users.noreply.github.com>
2023-03-11 17:09:50 +0200

    update

---
+++ src/main/java/main/features/Main.java
@@ -0,0 +1,17 @@
+package main.features;
+
+import lombok.SneakyThrows;
+import main.features.dao.ClientCrudService;
+import main.features.entity.Client;
+import main.features.migration.Migration;
+
+public class Main {
+    @SneakyThrows
+    public static void main(String[] args) {
+        Migration.migrate();
+        Client client = new Client("John Doe");
+        System.out.println("client.getId() = " + client.getId());
+        ClientCrudService service = new ClientCrudService();
+        service.save(client);
+    }
+}
